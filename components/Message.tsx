7ff09bdc6dc3b0e5863ca7a9d3faa2157d6effe6
saykimchhong <kimchhongsay2001@gmail.com>
2023-08-15 15:00:43 +0700

    update

--- components/Message.tsx
+++ components/Message.tsx
@@ -7,9 +7,10 @@ function Message({ message }: Props) {
   const isChatGpt = message.user.name === "ChatGPT";
   return (
     <div className={`py-5 text-white ${isChatGpt && "bg-[#434654]"}`}>
-      <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+      <div className="flex space-x-5 px-10 max-w-5xl mx-auto">
+        {/*eslint-disable-next-line @next/next/no-img-element */}
         <img src={message.user.avatar} alt="" className="w-8 h-8" />
-        <p className="pt-1 text-sm">{message.text}</p>
+        <p className="pt-1 text-md">{message.text}</p>
       </div>
     </div>
   );
--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,9 +1,8 @@
 import admin from 'firebase-admin';
 import { getApps } from 'firebase-admin/app';
 
-const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+
 
 if (!getApps().length) {
     admin.initializeApp({
